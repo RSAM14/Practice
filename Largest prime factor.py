d1425caf83460d6698c3bb3ef1e6f4ab06ecb3d5
RSAM14 <94971210+RSAM14@users.noreply.github.com>
2021-12-15 18:44:34 +0000

    Update Largest prime factor.py

--- Largest prime factor.py
+++ Largest prime factor.py
@@ -3,15 +3,6 @@ factor = 2
 
 prime_number = number_to_factorise
 
-"""
-list_of_factors =[]
-while factor < number_to_factorise:
-     if number_to_factorise%factor == 0:
-        list_of_factors.append(factor)
-    factor = factor + 1
-factor = 2
-print(f'list of factors {list_of_factors}')
-"""
 while factor*factor <= prime_number:
     while prime_number%factor == 0 :
         if prime_number/factor != 1:
